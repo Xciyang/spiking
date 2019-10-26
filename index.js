757b5c51286b465fedb653ecb7ff96ad38de98c1
Ciyang <896681584@qq.com>
2019-10-26 19:55:56 +0800

    fix : Fix input

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const request = require('request')
+const request = require('request');
+const fs = require('fs');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-
-// const MD5 = require("crypto-js/md5");
 
 class UrlTasks {
     constructor(url = '', flag = 1) {
@@ -10,14 +9,32 @@ class UrlTasks {
         this.waitQueue = new Array();
         this.flag = flag;
         this.waitToDeal = 0;
-        if (this.flag) this.firstUrl = new URL(url);
+        this.path = './';
+        this.errorQueue = new Array();
+        if (this.flag) {
+            try {
+                this.firstUrl = new URL(url);
+            } catch (e) {
+                console.log('I think your should restart the program');
+            }
+        }
         this.push(url);
     }
     push(url = '') {
+        try {
+            var u = new URL(url, this.firstUrl);
+            url = u.href;
+        } catch (error) {
+            console.log('error : ' + url);
+            return -1;
+        }
         if (this.urlSet.has(url)) return -1;
         this.urlSet.add(url);
         this.waitQueue.push(url);
         ++this.waitToDeal;
+    }
+    setPath(path = './') {
+        this.path = path;
     }
     // async waitForFinish() {
     //     return new Promise(resolve => {
@@ -52,15 +69,16 @@ class UrlTasks {
     async download(url = '', tasks) {
         return new Promise((resolve, reject) => {
             request(url, function (error, response, body) {
-                if (error) {
-                    resolve(0);
-                    return;
-                }
-                if (response.statusCode == 200) {
-                    if (response.headers['content-type'] == 'image/png') {
-
+                if (!error && response.statusCode == 200) {
+                    if (response.headers['content-type'].search('image') != -1) {
+                        try {
+                            var upath = new URL(url).pathname.split('/').join('_');
+                            request.get(url).pipe(fs.createWriteStream(tasks.path + '/' + upath));
+                        } catch (e) {
+                            console.log('error1');
+                        }
                     }
-                    else {
+                    else if (response.headers['content-type'].search('text') != -1) {
                         const dom = new JSDOM(body, {
                             url: url,
                             contentType: response.headers['content-type'],
@@ -78,10 +96,11 @@ class UrlTasks {
                         }
                     }
                     resolve(1);
+                    return;
                 }
-                else {
-                    resolve(0);
-                }
+                console.log('error : ' + url);
+                tasks.errorQueue.push(url);
+                resolve(0);
             });
         });
     }
@@ -93,20 +112,35 @@ class UrlTasks {
             if (res == 0) console.log('failed : ' + nowUrl);
             --this.waitToDeal;
         }
+        var res = this.errorQueue;
+        this.errorQueue = '';
+        return res;
     }
 }
-
 process.stdin.setEncoding('utf8');
 
-console.log('Input URL:');
-
-process.stdin.on('readable', () => {
-    programStart();
-});
-
-function programStart() {
-    var chunk = process.stdin.read();
-    var newTasks = new UrlTasks(chunk);
-    newTasks.workSingle();
-    console.log('任务启动！');
+function cutRFLF(str = '') {
+    return str ? str.replace(/[\r\n]/g, "") : '';
 }
+
+async function read() {
+    return new Promise((resolve, reject) => {
+        process.stdin.on('data', function (chunk) {
+            resolve(chunk);
+        })
+    });
+}
+function programStart() {
+    console.log('<1>Input URL');
+    read().then((res1) => {
+        var newTasks = new UrlTasks(cutRFLF(res1));
+        console.log('<2>Input Local Path');
+        read().then((res2) => {
+            newTasks.setPath(cutRFLF(res2));
+            newTasks.workSingle();
+            console.log('start');
+        });
+    });
+}
+
+programStart();
